335250abb731842a13ec5943a9f0ad7aab3550f5
kbiehler <klaus.biehler@stud-mail.uni-wuerzburg>
2025-02-20 11:46:30 +0100

    Init vite react-ts app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import uniWueLogo from "/uni-wuerzburg-logo.svg";
+
+function App() {
+  return (
+    <>
+      <img src={uniWueLogo} className="logo" alt="Universität Würzburg" />
+    </>
+  );
+}
+
+export default App;
