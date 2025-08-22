5af7f13033b5ca35a83f3ce4c14c92735c08381c
Klaus Biehler <klausbiehler@gmail.com>
2025-08-22 11:34:46 +0200

    FilterPanel

--- src/components/csv/NumberFilterPanel.tsx
+++ src/components/csv/NumberFilterPanel.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./NumberFilter.css";
 
 export class NumberFilterCfgDto {
-  filterType: "all" | "severity" | "probability" | "criticality" = "criticality";
+  filterType: "all" | "severity" | "probability" | "criticality" = "all";
   smallerGreaterEqual: ">=" | "<=" | "=" = ">=";
   filterNumber: number = 5;
 }
@@ -15,41 +15,33 @@ type Props = {
 const NumberCfgPanel: React.FC<Props> = ({ config, handleChange }) => {
   return (
     <div className="nf-row">
-  <label className="nf-label">Number Filter:</label>
+      <label className="nf-label">Number Filter:</label>
 
-  <select
-    className="nf-filter"
-    value={config.filterType}
-    onChange={(e) => handleChange("filterType", e.target.value)}
-  >
-    <option value="all">Show all</option>
-    <option value="severity">Severity Number</option>
-    <option value="probability">Probability Number</option>
-    <option value="criticality">Criticality Number</option>
-  </select>
-
-  {config.filterType !== "all" && (
-    <>
-      <select
-        className="nf-operator"
-        value={config.smallerGreaterEqual}
-        onChange={(e) => handleChange("smallerGreaterEqual", e.target.value)}
-      >
-        <option value=">=">&gt;=</option>
-        <option value="<=">&lt;=</option>
-        <option value="=">=</option>
+      <select className="nf-filter" value={config.filterType} onChange={(e) => handleChange("filterType", e.target.value)}>
+        <option value="all">Show all</option>
+        <option value="criticality">Criticality Number</option>
+        <option value="severity">Severity Number</option>
+        <option value="probability">Probability Number</option>
       </select>
 
-      <input
-        className="nf-value"
-        type="number"
-        value={config.filterNumber}
-        onChange={(e) => handleChange("filterNumber", parseFloat(e.target.value))}
-        placeholder="Value"
-      />
-    </>
-  )}
-</div>
+      {config.filterType !== "all" && (
+        <>
+          <select className="nf-operator" value={config.smallerGreaterEqual} onChange={(e) => handleChange("smallerGreaterEqual", e.target.value)}>
+            <option value=">=">&gt;=</option>
+            <option value="<=">&lt;=</option>
+            <option value="=">=</option>
+          </select>
+
+          <input
+            className="nf-value"
+            type="number"
+            value={config.filterNumber}
+            onChange={(e) => handleChange("filterNumber", parseFloat(e.target.value))}
+            placeholder="Value"
+          />
+        </>
+      )}
+    </div>
   );
 };
 
